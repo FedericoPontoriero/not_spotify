aaef5bbbd1aad59e548d6b1611aa27e19fd8ff6b
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-10-14 01:35:54 -0300

    music player functionality

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,7 +4,18 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, i }) => {
+const SongCard = ({ song, data, isPlaying, activeSong, i }) => {
+  const dispatch = useDispatch();
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
@@ -15,7 +26,13 @@ const SongCard = ({ song, i }) => {
               : "hidden"
           }`}
         >
-          <PlayPause />
+          <PlayPause
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+          />
         </div>
         <img alt="song_img" src={song.images?.coverart} />
       </div>
